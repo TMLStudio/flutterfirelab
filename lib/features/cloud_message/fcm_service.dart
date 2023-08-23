f9de29fcdbb3eaa619da1891007ec0f0fa256348
Tran Manh Linh <devngmo@gmail.com>
2023-08-24 06:51:51 +0700

    Update FCM Service: show push notification

--- lib/features/cloud_message/fcm_service.dart
+++ lib/features/cloud_message/fcm_service.dart
@@ -3,20 +3,35 @@ import 'dart:convert';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutterfirelab/features/push_notification/push_service.dart';
 import 'package:flutterfirelab/firebase_options.dart';
 
 class FCMService {
   String? token;
   String msgFromCloud = 'No message';
   Future initAsync() async {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
     token = await FirebaseMessaging.instance.getToken();
-
+    debugPrint("Client Token: $token");
     FirebaseMessaging messaging = FirebaseMessaging.instance;
 
     FirebaseMessaging.onMessage.listen((msg) {
-      msgFromCloud = "Sender=${msg.from} messageId=${msg.messageId} data=${jsonEncode(msg.data)} receiveAt=${DateTime.now().toIso8601String()}";
+      debugPrint("msg.data=${jsonEncode(msg.data)}");
+      String title = '';
+      String body = '';
+      if (msg.notification != null) {
+        title = "${msg.notification!.title}";
+        body = "${msg.notification!.body}";
+        debugPrint("msg.notification: title=$title body=$body");
+      }
+
+
+      msgFromCloud =
+          "title=${title} body=${body} Sender=${msg.from} messageId=${msg.messageId} data=${jsonEncode(msg.data)} receiveAt=${DateTime.now().toIso8601String()}";
       debugPrint(msgFromCloud);
+
+      pushService.showPushNotification(title, body, 'cloud');
     });
 
     ///Configure notification permissions
@@ -38,9 +53,7 @@ class FCMService {
       provisional: false,
       sound: true,
     );
-
   }
-
 }
 
 final fcm = FCMService();
